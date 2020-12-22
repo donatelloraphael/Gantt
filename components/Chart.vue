be5af1dac22dcff27fac36ae514eafc135fd74fc
Akbar <27790979+donatelloraphael@users.noreply.github.com>
2020-12-23 01:37:05 +0530

    initial layout

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,5 +1,16 @@
 <template>
-	
+	<div class="chart-container">
+		<div class="operations">
+			
+		</div>
+		<div class="components">
+			
+		</div>
+		<div class="edit-panel">
+			
+		</div>
+		
+	</div>
 </template>
 
 
@@ -11,5 +22,46 @@ export default {
 </script>
 
 <style scoped>
+
+.chart-container {
+	width: 90%;
+	height: auto;
+	min-height: 600px;
+	border: 2px solid #e2e2e2;
+	border-radius: 10px;
+	padding: 40px;
+	display: grid;
+  align-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  grid-gap: 2rem;
+}
+
+.operations {
+	height: 100%;
+	background-color: #f2f2f2;
+}
+
+.components {
+	height: 100%;
+	grid-column: span 2;
+	background-color: #f2f2f2;
+}
+
+.edit-panel {
+	height: 100%;
+	background-color: #f2f2f2;
+	/*display: none;*/
+}
 	
+@media(max-width: 1204px) {
+	.chart-container {
+  	grid-template-rows: repeat(2, 600px);
+	}
+}
+
+@media(max-width: 902px) {
+	.chart-container {
+  	grid-template-rows: repeat(3, 600px);
+	}
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,8 +25,6 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -41,4 +39,4 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   }
-}
+};
